eb80fbe434347dcc00039a4fad11c28df28a9ffe
arleenchr <13521059@mahasiswa.itb.ac.id>
2024-04-01 09:19:14 +0700

    Feat: Model & Vertex class

---
+++ src/vertex.js
@@ -0,0 +1,13 @@
+class Vertex{
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {Array<number>} color 
+     */
+    constructor(x, y, color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
